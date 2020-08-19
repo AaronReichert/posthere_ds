df25c7a26c44e95c6437f661d8fadfb409cb1ab0
AaronReichert <reichert.aaron.c@gmail.com>
2020-08-18 23:00:23 -0500

    Got it working

--- PostHere.py
+++ PostHere.py
@@ -1,22 +1,22 @@
 from flask import Flask
 
 
+app = Flask(__name__) 
+
 def create_app():
     '''create and configure the flask application'''
 
-    app = Flask(__name__)
+@app.route('/')
+def root():
+    message = 'Where should you post that on redit?'
+    return message
 
-    @app.route('/')
-    def root():
-        message = 'Where should you post that on redit?'
-        return message
+@app.route('/submit')
+def submit():
+    message = 'enter your post here'
+    return message
 
-    @app.route('/submit', methods=['POST'])
-    def submit():
-        message = 'enter your post here'
-        return message
-
-    @app.route('/suggestions', methods=['GET'])
-    def suggestions():
-        message = 'here are suggested posts'
-        return message
+@app.route('/suggestions')
+def suggestions():
+    message = 'here are suggested posts'
+    return message
