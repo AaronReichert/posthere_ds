2f2a5946a1703acb74583d74fe1ae77b46314c7c
Toby Chen <toby-chen@lambdastudents.comclear>
2020-08-20 23:54:16 -0400

    Added gitignore and LICENSE file

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+
 def create_app():
     '''Create and configure an instance of the Flask application'''
 
@@ -7,7 +8,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] =
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-    
+
     @app.route('/')
     def root():
         return 'Where should you post that on redit?'
@@ -19,5 +20,5 @@ def create_app():
     @app.route('/suggestions')
     def suggestions():
         return 'Here are suggested posts'
-    
+
     return app
